610ae89d7e9ccbbe287ea0d3771660f98c3029a0
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2023-01-16 21:01:34 +0100

    Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,26 +5,26 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-light " aria-label="Eighth navbar example">
       <div class="container">
-        <a class="navbar-brand" href="#">Container</a>
+        <a class="navbar-brand" href="#">Vaccitracker</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarsExample07">
           <ul class="navbar-nav col-lg-6 justify-content-lg-center">
-            <li class="nav-item mr-4">
+            <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item mr-4">
-              <a class="nav-link" href="#">Link</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About us</a>
             </li>
-            <li class="nav-item mr-4">
-              <a class="nav-link disabled">Disabled</a>
+            <li class="nav-item">
+              <a class="nav-link disabled">Contact us</a>
             </li>
           </ul>
           <div class="d-lg-flex col-lg-6 justify-content-lg-end">
-            <a class="nav-link" href="#">Login</a>
-            <button class="btn btn-primary">Button</button>
+            <a class="nav-link m-2" href="#">Login</a>
+            <button class="btn btn-primary">Get Started for free</button>
           </div>
         </div>
       </div>
