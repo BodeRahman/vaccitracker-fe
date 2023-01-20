fb17d98437693ee6e4b1f9b96763c3bbcedcc164
Lekan <jimoridwanlekan@gmail.com>
2023-01-20 12:23:30 +0100

    Add routing to project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import React, { Fragment } from 'react';
-import Newsletter from './components/Newsletter';
-import Footer from './components/Footer';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Fragment>
-      <Navbar />
-      <Home />
-      <Newsletter />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
     </Fragment>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,45 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/img/logo.png'
 import LoginButton from './LoginButton';
 import SignUpButton from './SignUpButton';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light " aria-label="Eighth navbar example">
+    <nav
+      className="navbar navbar-expand-lg bg-light "
+      aria-label="Eighth navbar example"
+    >
       <div className="container">
-        <a className="navbar-brand" href="#"><img src={logo} alt="Vaccitracker logo" /></a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+        <a className="navbar-brand" href="#">
+          <img src={logo} alt="Vaccitracker logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarsExample07"
+          aria-controls="navbarsExample07"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarsExample07">
-          <ul className="navbar-nav col-lg-6 justify-content-lg-center">
-          
-          </ul>
+          <ul className="navbar-nav col-lg-6 justify-content-lg-center"></ul>
           <div className="d-lg-flex col-lg-6 justify-content-lg-end">
-            <LoginButton  text="Login"></LoginButton>
-            <SignUpButton  text="Get started for free" type="submit"></SignUpButton>
+            <Link to="/login">
+              <LoginButton text="Login"></LoginButton>
+            </Link>
+            <Link to="/signup">
+              <SignUpButton
+                class="btn"
+                text="Get started for free"
+                type="submit"
+              ></SignUpButton>
+            </Link>
           </div>
         </div>
       </div>
