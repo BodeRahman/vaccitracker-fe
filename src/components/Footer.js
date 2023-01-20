dc944d1a1a80e67d3e96ecf8f3ad44374307ee9d
Lekan <jimoridwanlekan@gmail.com>
2023-01-20 12:03:15 +0100

    Add linters configuration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from '../assets/img/vaccitracker-logo-footer.png';
+import React from "react";
+import logo from "../assets/img/vaccitracker-logo-footer.png";
 
 const Footer = () => {
   return (
-    <div class="container-fluid footer">
-        <footer class="">
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <img className="img-fluid mt-3" src={logo} alt=""/>
-            </ul>
-            <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
-        </footer>
+    <div className="container-fluid footer">
+      <footer className="">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <img className="img-fluid mt-3" src={logo} alt="" />
+        </ul>
+        <p className="text-center text-muted">&copy; 2022 Company, Inc</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,15 +3,15 @@ import SignUpButton from './SignUpButton';
 
 const Newsletter = () => {
   return (
-    <section class="py-5 text-center container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="content mt-2">
+    <section className="py-5 text-center container">
+        <div className="row">
+            <div className="col-sm-12">
+                <div className="content mt-2">
                     <h2>Get insights about Different Vaccinations </h2>
                     <p>Join our email list to receive immunization reminders, support articles, tips, and more.</p>
-                    <div class="input-group col-md-8">
-                        <input type="email" class="form-control inp" placeholder="Enter your email"/>
-                        <span class="input-group-btn">
+                    <div className="input-group col-md-8">
+                        <input type="email" className="form-control inp" placeholder="Enter your email"/>
+                        <span className="input-group-btn">
                             <SignUpButton class="btn" text="Suscribe Now" type="submit"></SignUpButton>
                         </span>
                     </div>
