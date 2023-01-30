cc45626a20f7d1e5096a0745cd6aa5955bf0b849
Lekan <jimoridwanlekan@gmail.com>
2023-01-30 22:14:45 +0100

    Display children

--- src/pages/TempHome.jsx
+++ src/pages/TempHome.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { fetchChildren } from '../features/child/childSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import Sidebar from '../components/Sidebar';
-import avatar from '../assets/img/child-pic.png'
-import vacci1 from '../assets/img/measles.png'
-import vacci2 from '../assets/img/pvv.png'
-
+import React from "react";
+import { useEffect } from "react";
+import { fetchChildren, selectChildren } from "../features/child/childSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Sidebar from "../components/Sidebar";
+import avatar from "../assets/img/child-pic.png";
+import vacci1 from "../assets/img/measles.png";
+import vacci2 from "../assets/img/pvv.png";
 
 const TempHome = () => {
   const dispatch = useDispatch();
-  const 
+  const children = useSelector(selectChildren);
+  const displayedChildren = children.slice(0, 2);
+
+  useEffect(() => {
+    dispatch(fetchChildren());
+  }, [dispatch]);
+
   return (
     <>
       <div className="row gilroy">
@@ -20,9 +25,17 @@ const TempHome = () => {
         <div className="col-sm-7 col-md-9">
           <div className="row">
             <div className="col-sm-6 col-md-7">
-              <div className="mt-5 d-flex justify-content-between" style={{color: '#032F5B;'}}>
+              <div
+                className="mt-5 d-flex justify-content-between"
+                style={{ color: "#032F5B;" }}
+              >
                 <p className="fs-5 fw-bold">Upcoming vaccinations</p>
-                <a href='/reminders' className="text-decoration-none mt-2 text-dark">See all</a>
+                <a
+                  href="/reminders"
+                  className="text-decoration-none mt-2 text-dark"
+                >
+                  See all
+                </a>
               </div>
               <div className="row mt-2">
                 <div className="col-md-6 mt-3">
@@ -132,79 +145,65 @@ const TempHome = () => {
             </div>
 
             <div className="col-sm- col-md-4">
-              <div className="mt-5 d-flex justify-content-between" style={{color: '#032F5B'}}>
+              <div
+                className="mt-5 d-flex justify-content-between"
+                style={{ color: "#032F5B" }}
+              >
                 <p className="fs-5 fw-bold">Children and wards</p>
-                <a href='/children' className="text-decoration-none mt-2 text-dark" >See all</a>
-              </div>
-              <div className="col-md-12 mt-4">
-                <div
-                  className="card text-white"
-                  style={{ background: "#2F659D" }}
+                <a
+                  href="/children"
+                  className="text-decoration-none mt-2 text-dark"
                 >
-                  <div className="row">
-                    <div className="col-3 m-2 mt-5">
-                      <img
-                        className="rounded-circle card-img-left "
-                        src={avatar}
-                        style={{ width: "72px" }}
-                        alt="avatar1"
-                      />
-                    </div>
-                    <div className="col-8">
-                      <div className="card-body">
-                        <p className="card-title fw-bold">
-                          Name: Destiny Tunde
-                        </p>
-                        <p className="card-subtitle fs-6 fw-bolder">
-                          DOB: 23 February, 2022
-                        </p>
-                        <p className="card-text fw-light">Gender: Male</p>
-                        <p className="card-subtitle fs-6 fw-bolder">
-                          Country: Nigeria
-                        </p>
+                  See all
+                </a>
+              </div>
+              {displayedChildren.map((child) => (
+                <div key={child.id} className="col-md-12 mt-4">
+                  <div
+                    className="card text-white"
+                    style={{ background: "#2F659D" }}
+                  >
+                    <div className="row">
+                      <div className="col-3 m-2 mt-3">
+                        <img
+                          className="rounded-circle card-img-left "
+                          src={child.avatar_url}
+                          style={{ width: "72px" }}
+                          alt="avatar1"
+                        />
+                      </div>
+                      <div className="col-8">
+                        <div className="card-body">
+                          <p className="card-title fw-bold">
+                            Name: {child.first_name + " " + child.last_name}
+                          </p>
+                          <p className="card-subtitle fs-6 fw-bolder">
+                            DOB: {child.date_of_birth}
+                          </p>
+                          <p className="card-text fw-light">
+                            Gender: {child.gender}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              ))}
 
-              <div className="col-md-12 mt-4">
-                <div
-                  className="card text-white"
-                  style={{ background: "#2F659D" }}
-                >
-                  <div className="row">
-                    <div className="col-3 m-2 mt-5">
-                      <img
-                        className="rounded-circle card-img-left "
-                        src={avatar}
-                        style={{ width: "72px" }}
-                        alt="avatar1"
-                      />
-                    </div>
-                    <div className="col-8">
-                      <div className="card-body">
-                        <p className="card-title fw-bold">
-                          Name: Destiny Tunde
-                        </p>
-                        <p className="card-subtitle fs-6 fw-bolder">
-                          DOB: 23 February, 2022
-                        </p>
-                        <p className="card-text fw-light">Gender: Male</p>
-                        <p className="card-subtitle fs-6 fw-bolder">
-                          Country: Nigeria
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
 
             <div className="col-sm-6 col-md-11">
-              <div className="mt-5 d-flex justify-content-between" style={{color: '#032F5B;'}}>
+              <div
+                className="mt-5 d-flex justify-content-between"
+                style={{ color: "#032F5B;" }}
+              >
                 <p className="fs-5 fw-bold">Vaccination wiki</p>
-                <a href='/vaccination-wiki' className="text-decoration-none mt-2 text-dark">See all</a>
+                <a
+                  href="/vaccination-wiki"
+                  className="text-decoration-none mt-2 text-dark"
+                >
+                  See all
+                </a>
               </div>
               <div className="row mt-2">
                 <div className="col-md-6 my-3">
@@ -228,9 +227,7 @@ const TempHome = () => {
                             vaccine protects against diseases, mumps, and
                             rubella.
                           </p>
-                          <p className="card-text fw-bold mt-2">
-                            Read more
-                          </p>
+                          <p className="card-text fw-bold mt-2">Read more</p>
                         </div>
                       </div>
                     </div>
@@ -256,9 +253,7 @@ const TempHome = () => {
                             vaccine protects against diseases, mumps, and
                             rubella.
                           </p>
-                          <p className="card-text fw-bold mt-2">
-                            Read more
-                          </p>
+                          <p className="card-text fw-bold mt-2">Read more</p>
                         </div>
                       </div>
                     </div>
@@ -271,6 +266,6 @@ const TempHome = () => {
       </div>
     </>
   );
-}
+};
 
-export default TempHome
+export default TempHome;
