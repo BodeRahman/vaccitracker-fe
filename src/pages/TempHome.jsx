db4a4e4600fc21ea1588314e1b8b95e8638cfa04
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2023-01-31 12:47:43 +0100

    Add modal to Vaccination card

--- src/pages/TempHome.jsx
+++ src/pages/TempHome.jsx
@@ -224,9 +224,33 @@ const TempHome = () => {
                             vaccine protects against diseases, mumps, and
                             rubella.
                           </p>
-                          <p className="card-text fw-bold mt-2">
-                            Read more
-                          </p>
+                          <button type="button" className="btn card-text fw-bold mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Read More
+                          </button>
+
+                          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                              <div className="modal-content">
+                                <div className="modal-header">
+                                  <h1 className="modal-title fs-5" id="exampleModalLabel">Measles Vaccine</h1>
+                                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                  Measles can be prevented with MMR vaccine. The
+                                  vaccine protects against diseases, mumps, and
+                                  rubella.Measles can be prevented with MMR vaccine. The
+                                  vaccine protects against diseases, mumps, and
+                                  rubella.Measles can be prevented with MMR vaccine. The
+                                  vaccine protects against diseases, mumps, and
+                                  rubella.
+                                </div>
+                                <div className="modal-footer">
+                                  <button type="button" className="btn btn-flat" data-bs-dismiss="modal">Close</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          
                         </div>
                       </div>
                     </div>
@@ -252,9 +276,32 @@ const TempHome = () => {
                             vaccine protects against diseases, mumps, and
                             rubella.
                           </p>
-                          <p className="card-text fw-bold mt-2">
-                            Read more
-                          </p>
+                          <button type="button" className="btn card-text fw-bold mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Read More
+                          </button>
+
+                          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                              <div className="modal-content">
+                                <div className="modal-header">
+                                  <h1 className="modal-title fs-5" id="exampleModalLabel">Measles Vaccine</h1>
+                                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                  Measles can be prevented with MMR vaccine. The
+                                  vaccine protects against diseases, mumps, and
+                                  rubella.Measles can be prevented with MMR vaccine. The
+                                  vaccine protects against diseases, mumps, and
+                                  rubella.Measles can be prevented with MMR vaccine. The
+                                  vaccine protects against diseases, mumps, and
+                                  rubella.
+                                </div>
+                                <div className="modal-footer">
+                                  <button type="button" className="btn btn-flat" data-bs-dismiss="modal">Close</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
