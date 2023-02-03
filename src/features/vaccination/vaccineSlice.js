3db62d6e3c232fb1ddb3b642ef91a033dd33e673
Lekan <jimoridwanlekan@gmail.com>
2023-02-03 19:30:44 +0100

    Add vaccination actions get and update

---
+++ src/features/vaccination/vaccineSlice.js
@@ -0,0 +1,91 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import vaccineService from "./vaccineService";
+
+const initialState = {
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const fetchVaccines = createAsyncThunk(
+  "vaccine/fetchVaccines",
+  async (filter, wardId, thunkAPI) => {
+    try {
+      return await vaccineService.fetchVaccines(filter, wardId);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const updateVaccine = createAsyncThunk(
+  "vaccine/updateVaccine",
+  async (vaccineData, wardId, id, thunkAPI) => {
+    try {
+      return await vaccineService.updateVaccine(vaccineData, wardId, id);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const vaccineSlice = createSlice({
+  name: "vaccine",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchVaccines.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateVaccine.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchVaccines.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.message = action.payload;
+      })
+      .addCase(updateVaccine.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.message = action.payload;
+      })
+      .addCase(updateVaccine.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(fetchVaccines.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = vaccineSlice.actions;
+export const selectVaccine = (state) => state.vaccine;
+export default vaccineSlice.reducer;
+
