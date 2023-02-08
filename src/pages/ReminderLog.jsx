5ca4ae0a6460e25cc699150f4fadfb8eb5f034ed
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2023-02-08 03:07:27 +0100

    Fix reminder log card

--- src/pages/ReminderLog.jsx
+++ src/pages/ReminderLog.jsx
@@ -4,20 +4,20 @@ import avatar from '../assets/img/child-pic.png'
 const ReminderLog = () => {
   return (
     <>
-        <div className="row gilroy">
-            <div className="col-sm-5 col-md-3">
+        <div className="row gilroy-light">
+            <div className="col- col-sm-6 col-md-3">
                 <Sidebar />
             </div>
-            <div className="col-sm-7 col-md-9">
+            <div className="col-12 col-sm-12 col-md-9">
                 <div className="row">
-                    <div className="col-sm- col-md-12">
-                        <div className="mt-5 d-flex justify-content-between" style={{color: '#032F5B;'}}>
-                            <p className="fs-5 fw-bold">Upcoming vaccinations</p>
+                    <div className="col-11 col-sm-11 col-md- mx-1 justify-content-center">
+                        <div className="mt-5 d-flex justify-content-between">
+                            <p className="title-head">Upcoming vaccinations</p>
                         </div>
                         <a href='/reminders' className="rem">Set reminders</a>
                         <div className="row mt-2">
-                            <div className="col-md-4 mt-3">
-                                <div className="card card-deet">
+                            <div className="col-12 col-sm-6 col-md-4 mt-3">
+                                <div className="card" id="card-deet">
                                     <div className="row">
                                         <div className="col-3 m-2 mt-5">
                                             <img
@@ -42,8 +42,8 @@ const ReminderLog = () => {
                                 </div>
                             </div>
 
-                            <div className="col-md-4 mt-3">
-                                <div className="card card-deet">
+                            <div className="col-12 col-sm-6 col-md-4 mt-3">
+                                <div className="card" id="card-deet">
                                     <div className="row">
                                         <div className="col-3 m-2 mt-5">
                                             <img
@@ -68,8 +68,8 @@ const ReminderLog = () => {
                                 </div>
                             </div>
 
-                            <div className="col-md-4 mt-3">
-                                <div className="card card-deet">
+                            <div className="col-12 col-sm-6 col-md-4 mt-3">
+                                <div className="card" id="card-deet">
                                     <div className="row">
                                         <div className="col-3 m-2 mt-5">
                                             <img
